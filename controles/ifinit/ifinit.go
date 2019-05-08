5eafc2044a8237c9ebbfdc87bd65551b0cd8e432
lcamilo <lhenriquegomescamilo@gmail.com>
2019-05-07 21:06:42 -0300

    Trabalhando com if com inicalização de variável

---
+++ controles/ifinit/ifinit.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	if i := numeroAleatorio(); i > 5 { // também é suportado no switch
+		fmt.Println("Ganhou")
+	} else {
+		fmt.Println("Perdeu")
+	}
+
+}
+
+func numeroAleatorio() int {
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
+	return r.Intn(10)
+
+}
